676ce122352042fba597e09f17c67fc03c52bf70
Federico Zeitun <68887478+FedericoZeitun@users.noreply.github.com>
2021-03-09 21:41:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         const consultarAPI = async ()=>{
             if(consultar){
                 const appId = 'e591da1d632b8dd0e5bcc93a1e7e2fc4';
-                const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+                const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
 
                 const respuesta = await fetch(url);
                 const resultado = await respuesta.json()
